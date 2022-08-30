b8b4614ffb6619e783872a4f29b0231493996d47
Christian Lisangola <jochri3@gmail.com>
2022-08-30 12:41:39 +0300

    Add prototypes

--- 1-poo-js/function-constructeur.js
+++ 1-poo-js/function-constructeur.js
@@ -4,10 +4,35 @@ function Article(id, designation, description, prixHT) {
   this.designation = designation;
   this.description = description;
   this.prixHT = prixHT;
+
+  //   Mauvaise pratique car  chaque va avoir une copie des 2 méthodes
+  //   this.calculPrixTTC = function () {
+  //     return this.prixHT * 1.2;
+  //   };
+
+  //   this.afficherArticle = function () {
+  //     console.log(`Nom : ${this.designation}
+  //   Description : ${this.description}
+  //   ${this.prixHT} €
+  //   `);
+  //   };
 }
+
+Article.prototype.calculPrixTTC = function () {
+  return this.prixHT * 1.2;
+};
+
+Article.prototype.afficherArticle = function () {
+  console.log(`Nom : ${this.designation}
+    Description : ${this.description}
+    ${this.prixHT} €
+    `);
+};
+
+// const article = Article(1, "PS 5", "Meilleur console", 400);Mauvaise pratique
 
 const article = new Article(1, "PS 5", "Meilleur console", 400);
 const article2 = article; //article et article2 pointent vers le même objet en mémoire
 
 // const article = new Article(1, "PS 5", "Meilleur console", 400);
-// const article2 = new Article(2, "Xbox one", "Produit de Microsoft", 320);
+const article3 = new Article(2, "Xbox one", "Produit de Microsoft", 320);
